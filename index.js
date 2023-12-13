bc82589251f37bbf2531690addc1f70bdebe7367
Brandon <beegee54321@gmail.com>
2023-12-13 00:02:21 -0800

    need to debug index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class SVG {
         this.textVar = ''
         this.shapeVar = ''
     } render() {
-        return ``
+        return <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>
     }
     setText(text,color) {
         this.textVar = `<text x='150' y='125' font-size='60' text-anchor='middle' fill=${color}>${text}</text>`
@@ -38,6 +38,7 @@ function promptInput () {
             type: 'list',
             name: 'shape',
             message: 'What shape would you like your logo?',
+            choices: ['Circle', 'Square', 'Triangle'],
         },
     ])
 .then((data) => {
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -3,17 +3,20 @@ const { Circle, Square, Triangle } = require('./lib/shapes.js');
 describe('Circle test', () => {
     test('should render shape', () => {
         const circle = new Circle('blue');
+        expect(circle.render()).toEqual('<circle cx="160" cy="120" r="70" fill="blue"/>');
     })
 })
 
 describe('Square test', () => {
     test('should render shape', () => {
         const square = new Square('red');
+        expect(square.render()).toEqual('<rect x="69" y="52" width="150" height="170" fill="red"/>');
     })
 })
 
 describe('Triangle test', () => {
     test('should render shape', () => {
         const triangle = new Triangle('green');
-    })
-})
+        expect(triangle.render()).toEqual('<polygon height="160" width="140" points="140, 25 251, 190 61, 190" fill="green"/>');
+    });
+});
