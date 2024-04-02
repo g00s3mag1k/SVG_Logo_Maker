24a5f3542504fb50f2ea37dbb0264d3ab82cff37
Brandon <beegee54321@gmail.com>
2024-04-01 22:55:43 -0700

    saving code progress

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -3,8 +3,8 @@ const { Circle, Square, Triangle } = require('../lib/shapes');
 describe('Circle test', () => {
     test('should render shape with custom color', () => {
         const circle = new Circle();
-        circle.setColor('blue');
-        expect(circle.render()).toEqual(`<circle cx="160" cy="120" r="70" fill="blue"/>`);
+        circle.setColor("blue");
+        expect(circle.render()).toEqual(`<circle cx="146" cy="120" r="70" fill="blue"/>`);
     });
 });
 
@@ -12,7 +12,7 @@ describe('Square test', () => {
     test('should render shape with custom color', () => {
         const square = new Square();
         square.setColor('red');
-        expect(square.render()).toEqual(`<rect x="69" y="52" width="150" height="170" fill="${this.color}"/>`);
+        expect(square.render()).toEqual(`<rect x="70" y="43" width="150" height="150" fill="red"/>`);
     });
 });
 
@@ -20,6 +20,6 @@ describe('Triangle test', () => {
     test('should render shape with custom color', () => {
         const triangle = new Triangle();
         triangle.setColor('green');
-        expect(triangle.render()).toEqual(`<polygon points="140, 25 251, 190 61, 190" fill="${this.color}"/>`);
+        expect(triangle.render()).toEqual(`<polygon points="140, 25 240, 190 50, 190" fill="green"/>`);
     });
 });
